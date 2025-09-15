90032a99c3606b342a140b5b75f4b9446651ee70
Syed Bilal Alam <syedbilalalam2006@gmail.com>
2025-09-15 18:59:24 +0500

    Feat: All transactions now persists even after page reloads

--- src/Context/GlobalState.js
+++ src/Context/GlobalState.js
@@ -1,12 +1,43 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer, useEffect } from 'react';
 
 //Reducer
 import AppReducer from './AppReducer';
 
-// Create the initial state
-const initialState = {
-    transactions: []
+// Update session
+const updateSession = (transactionHistory) => {
+
+    localStorage.setItem(
+        'transactionHistory',
+        JSON.stringify(transactionHistory)
+    );
+
 }
+
+// Create the initial state
+const initialState = (() => {
+
+    let initialState = {
+        transactions: []
+    }
+
+    // Checking for previous sessions
+    const previousSession = localStorage.getItem('transactionHistory');
+    if (previousSession) {
+        try {
+            const previousSessionData = JSON.parse(previousSession);
+            initialState = previousSessionData;
+        }
+        catch {
+            updateSession(initialState);
+        }
+    }
+    else {
+        updateSession(initialState);
+    }
+
+    return initialState;
+
+})();
 
 // Create the Global Context
 export const GlobalContext = createContext(initialState);
@@ -30,8 +61,13 @@ export const GlobalProvider = ({ children }) => {
             dispatch({
                 type: 'ADD_TRANSACTION',
                 payload: transaction
-            })
+            });
         }
+
+        // Saving state to the session
+        useEffect(()=>{
+            updateSession(state);
+        }, [state]);
 
     return (
         <GlobalContext.Provider value={
